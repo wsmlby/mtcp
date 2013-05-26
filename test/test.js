7346a90767585ff5371e11e1d63de22e7b4332b9
wsmlby <wsmlby@gmail.com>
2013-05-26 18:37:46 +0800

    partially usable

--- test/test.js
+++ test/test.js
@@ -3,8 +3,10 @@ var mtcp = require('../lib/mtcp');
 
 var host="2620:107:300f::b8a9:aee7";
 host="127.0.0.1";
-require('./pipeserver')
+
+
+console.log("forward locathost 10029 to "+host+ "9999");
+require('../lib/pipeserver')
     .startServer(net,mtcp,8003,host,10029);
-require('./pipeserver')
-    .startServer(mtcp,net,9999,"127.0.0.1",8003);
+
 
