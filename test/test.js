f94dc232f1ba4cca26eaac0fb72bad3a4f081aa5
wsmlby <wsmlby@gmail.com>
2013-05-27 22:38:47 +0800

    package name to mtcp

--- test/test.js
+++ test/test.js
@@ -1,8 +1,9 @@
 var net = require('net');
 var mtcp = require('../lib/mtcp');
 
-var host="2620:107:300f::b8a9:aee7";
+var host="2620:107:300f::b8a9:b32f";
 host="127.0.0.1";
+host="www.wsmlby.info";
 
 
 console.log("forward locathost 10029 to "+host+ "9999");
